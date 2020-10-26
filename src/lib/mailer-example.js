08f61d2b22c3a1d191836440b6a0ec84d2b922d7
Everton Pavan <everton.pavan@sertao.ifrs.edu.br>
2020-10-26 16:35:14 -0300

    first commit

--- src/lib/mailer-example.js
+++ src/lib/mailer-example.js
@@ -4,7 +4,7 @@ module.exports = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "87fdcdaf9ff049",
-      pass: "cde951791e748b"
+      user: "",
+      pass: ""
     }
 })
